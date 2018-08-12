30f383b53629a02365af07971cb18dd7dbab1b0a
Alexandra Keenan <alexandra.b.keenan@gmail.com>
2018-08-12 12:19:44 -0400

    Fixed loadMACxls() to better accomodate comments in raw files.

--- R/load_data.R
+++ R/load_data.R
@@ -1,5 +1,8 @@
 #returns MACS xls object
 loadMACSxls = function(filename){
-  header_line = 28
-  return(read.table(filename, sep = "\t", header = T, skip = header_line-1))
+  df = read.table(filename, sep = "\t", header = T,
+    comment.char = "#", stringsAsFactors = F, quote="")
+  colnames(df) = as.character(df[1,])
+  df = df[-1,]
+  return(df)
 }
